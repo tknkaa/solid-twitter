657cca3007d5e7486c9a36fc8ede6d57d108acff
tknkaa <tknkaa@users.noreply.github.com>
2026-04-25 21:29:22 +0800

    hardcode local database path

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,3 +1,3 @@
-import { drizzle } from "drizzle-orm/libsql"
+import { drizzle } from "drizzle-orm/libsql";
 
-export const db = drizzle(process.env.DATABASE_URL!)
+export const db = drizzle("file:local.db");
